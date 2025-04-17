faad68e5e5eabe205b35b112c169bd80ad2cefeb
Errold Núñez <errold222@gmail.com>
2025-04-16 20:54:44 -0600

    Reglas de jwt

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,4 +1,4 @@
-import jwt, { JwtPayload } from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 
 interface JwtPayloadData {
     [key: string]: any; // Adjust this to match the expected structure of your payload
@@ -6,6 +6,7 @@ interface JwtPayloadData {
 
 export const generateJWT = (payload: JwtPayloadData): string => {
     if (!process.env.JWT_SECRET) {
+        console.log("JWT_SECRET:", process.env.JWT_SECRET); // Esto debería imprimir el valor
         throw new Error('JWT_SECRET is not defined in environment variables');
     }
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
