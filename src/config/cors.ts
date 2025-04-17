894a07a64fdd1b618399c7d62d0f700434f8649b
Errold Núñez <errold222@gmail.com>
2025-04-16 18:33:22 -0600

    Segunda actualización en las reglas de cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,20 +1,20 @@
-import { CorsOptions } from 'cors'
+interface CorsConfig {
+    origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => void;
+}
 
-export const corsConfig : CorsOptions = {
-    origin: function(origin, callback) {
-        const whiteList = [
+export const corsConfig: CorsConfig = {
+    origin: function(origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void): void {
+        console.log("Origin recibido:", origin);
+        const whiteList: (string | undefined)[] = [
             process.env.FRONTEND_URL,
             'https://aesthetic-heliotrope-cde61c.netlify.app'
         ]
-        
-        if(process.argv[2] === '--api') {
-            whiteList.push(undefined)
-        }
 
-        if(whiteList.includes(origin)) {
-            callback(null, true)
+        if (whiteList.includes(origin)) {
+            callback(null, true);
         } else {
-            callback(new Error('Error de CORS'))
+            console.error("CORS bloqueado para origin:", origin);
+            callback(new Error('Error de CORS'));
         }
     }
-}
+};
